809562f793b77210413e5f681dd2dfcdd09d123d
Rahul <rahul@bigappcompany.in>
2019-07-30 10:15:16 +0530

    added click events and state vars

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from 'react';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I am a React Developer</h1>
-    </div>
-  );
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+	state = {
+		persons : [
+			{name : 'Rahul', age : 22},
+			{name : 'Neha', age : 24},
+			{name : 'Swathi', age : 21},
+		]
+	}
+
+	switchNameHandler = () => {
+		this.setState({
+			persons : [
+				{name : 'Rahul Mathews', age : 22},
+				{name : 'Neha', age : 24},
+				{name : 'Swathi', age : 22},
+			]
+		})
+	}
+
+	render(){
+		return (
+		<div className="App">
+			<h1>Hi, I am a React Developer</h1>
+			<Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+			<Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies:Photography</Person>
+			<Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+			<button onClick={this.switchNameHandler}>Switch Name</button>
+		</div>
+		);
+	}
 }
 
 export default App;
