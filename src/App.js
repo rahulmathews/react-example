024b7209d462ebbd88369c4176f39a87281df756
Rahul <rahul@bigappcompany.in>
2019-08-01 09:09:12 +0530

    Added inline styles and styling with radium

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Radium from 'radium';
 
 import './App.css';
 import Person from './Person/Person';
@@ -45,10 +46,15 @@ class App extends Component {
 
 	render(){
 		const style = {
-			backgroundColor : 'salmon',
+			backgroundColor : 'green',
+			color : 'white',
 			border : '1px solid blue',
 			cursor : 'pointer',
-			padding : '16px'
+			padding : '16px',
+			':hover' : {
+				backgroundColor : 'lightgreen',
+				color : 'black'
+			}
 		}
 
 		let persons = null;
@@ -66,11 +72,25 @@ class App extends Component {
 					})}
 				</div>
 			);
+			style.backgroundColor = 'red';
+			style[':hover'] = {
+				backgroundColor : 'salmon',
+				color : 'black'
+			}
+		}
+
+		let classes = [];
+		if(this.state.persons.length <= 2){
+			classes.push('red');
+		}
+		if(this.state.persons.length <= 1){
+			classes.push('bold');
 		}
 
 		return (
 		<div className="App">
 			<h1>Hi, I am a React Developer</h1>
+			<p className={classes.join(' ')}>This is really working</p>
 			<button style={style} onClick={this.togglePersonsHandler}>Toggle persons</button>
 			{persons}
 		</div>
@@ -78,4 +98,4 @@ class App extends Component {
 	}
 }
 
-export default App;
+export default Radium(App);
