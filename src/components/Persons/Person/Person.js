d105781f172965a1607f62ca205db0c65310b72e
Rahul <rahul@bigappcompany.in>
2019-08-07 23:17:47 +0530

    added higher order component and added exceptions for react

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -27,6 +27,11 @@ class Person extends Component{
                 <input type="text" onChange={this.props.change} value={this.props.name}></input>
             </div>
         )
+        // return [
+        //     <p key="1" onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} years old</p>,
+        //     <p key="2">{this.props.children}</p>,
+        //     <input key="3" type="text" onChange={this.props.change} value={this.props.name}></input>
+        // ]
     }
 }
 
